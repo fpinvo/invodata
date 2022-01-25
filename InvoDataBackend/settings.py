77368dff5eb0bf8885a202dc0e278c8c9deff625
Harib <harib.siddique@invozone.com>
2022-01-25 18:08:07 +0500

    Add email cred in .env file

--- InvoDataBackend/settings.py
+++ InvoDataBackend/settings.py
@@ -184,9 +184,9 @@ SWAGGER_SETTINGS = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nitrrrx@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pakistan_1824'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
